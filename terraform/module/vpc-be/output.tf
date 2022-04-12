3271d0c727033e44ea316e13da0a5064caba3157
Jeff Yen <jeff51419@gmail.com>
2022-04-12 15:54:54 +0800

    modify the nat public ip

--- terraform/module/vpc-be/output.tf
+++ terraform/module/vpc-be/output.tf
@@ -11,7 +11,7 @@ output "VPC-BE" {
     "06_Public_Subnet_IDs"          = module.vpc-be.public_subnets
     "07_Public_Subnet_CIDR_Blocks"  = module.vpc-be.public_subnets_cidr_blocks
     "08_Internet_Gateway_ID"        = module.vpc-be.igw_id
-    "09_NAT_Public_IP(s)"           = module.vpc.nat_public_ips
+    "09_NAT_Public_IP(s)"           = module.vpc-be.nat_public_ips
     "10_NAT_Gateway_ID(s)"          = module.vpc-be.natgw_ids
     "domain"                        = var.domain
   }
