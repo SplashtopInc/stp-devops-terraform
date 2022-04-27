fcc2a8ec8026f32189777fdaea5ecb2330fdd587
jeff51419 <jeff51419@gmail.com>
2022-04-27 12:27:24 +0800

    delete v3.12 version module

--- terraform/module/atlantis-v3.12/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  shared_credentials_files = ["~/.aws/credentials"]
-  region                   = var.region
-  profile                  = var.profile
-  assume_role {
-    role_arn = "%{if var.assume_role != ""}${var.assume_role}%{endif}"
-  }
-}
