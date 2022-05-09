4bd3a39663709710cf4fe2f88b5f434458464f6f
jeff51419 <jeff51419@gmail.com>
2022-05-09 12:25:23 +0800

    add acm module

---
+++ terraform/module/acm-v3.4.1/main.tf
@@ -0,0 +1,39 @@
+locals {
+  # Use existing (via data source) or create new zone (will fail validation, if zone is not reachable)
+  use_existing_route53_zone = true
+
+  domain = var.domain_name
+
+  # Removing trailing dot from domain - just to be sure :)
+  domain_name = trimsuffix(local.domain, ".")
+}
+
+data "aws_route53_zone" "this" {
+  count = local.use_existing_route53_zone ? 1 : 0
+
+  name         = local.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_zone" "this" {
+  count = !local.use_existing_route53_zone ? 1 : 0
+  name  = local.domain_name
+}
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "3.4.1"
+
+  domain_name = local.domain_name
+  zone_id     = coalescelist(data.aws_route53_zone.this.*.zone_id, aws_route53_zone.this.*.zone_id)[0]
+
+  subject_alternative_names = var.subject_alternative_names
+
+  wait_for_validation = var.wait_for_validation
+
+  tags = {
+    Name        = local.domain_name
+    Project     = var.project
+    Environment = var.environment
+  }
+}
