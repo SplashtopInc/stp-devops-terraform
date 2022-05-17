d729753232949dfad8c8a2e071f2a61c07242f6a
jeff51419 <jeff51419@gmail.com>
2022-05-17 17:08:57 +0800

    create v18 eks be-app module

---
+++ terraform/module/eks-v18/be-app/data.tf
@@ -0,0 +1,43 @@
+data "aws_caller_identity" "current" {}
+
+### get ami info
+data "aws_ami" "beapp_node_ami" {
+  most_recent = true
+  owners      = [local.worker_ami_owner_id]
+
+  filter {
+    name   = "name"
+    values = [local.worker_ami_linux]
+  }
+}
+### get vpc info
+data "aws_vpc" "stp-vpc-pub" {
+  tags = {
+    Name = "stp-vpc-pub-${var.region}"
+  }
+}
+
+# data "aws_subnet_ids" "stp-vpc-pub-private" {
+#   vpc_id = data.aws_vpc.stp-vpc-pub.id
+
+#   filter {
+#     name = "tag:Name"
+#     values = [
+#       "stp-vpc-pub-private-${var.region}*"
+#     ]
+#   }
+# }
+
+data "aws_subnets" "stp-vpc-pub-private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.stp-vpc-pub.id]
+  }
+
+  filter {
+    name = "tag:Name"
+    values = [
+      "stp-vpc-pub-private-${var.region}*"
+    ]
+  }
+}
