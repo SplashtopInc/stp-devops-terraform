1e0f1aa74d2756d343a9792634c71764f3b65836
jeff51419 <jeff51419@gmail.com>
2022-06-06 15:28:06 +0800

    upgrade aws provide to 4.0.0

--- terraform/module/vpc-db/backend.tf
+++ terraform/module/vpc-db/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket  = "mybucket"
-    key     = "path/to/my/key"
-    region  = var.region
-    encrypt = true
-    profile = var.profile
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket  = "mybucket"
+#     key     = "path/to/my/key"
+#     region  = var.region
+#     encrypt = true
+#     profile = var.profile
+#   }
+# }
