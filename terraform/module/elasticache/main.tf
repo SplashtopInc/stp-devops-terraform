07869a36de578b62fc8e063dbc72d545e77dc37a
jeff51419 <jeff51419@gmail.com>
2022-07-21 11:30:28 +0800

    remove Deprecated var

--- terraform/module/elasticache/main.tf
+++ terraform/module/elasticache/main.tf
@@ -27,6 +27,18 @@ resource "aws_security_group" "redis-sg" {
     ]
   }
 
+  egress {
+    description = "Allow all to local LAN"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16"
+    ]
+  }
+
   tags = {
     Name = "${var.redis_nodes_list[count.index]}-${local.cluster_name_suffix}-sg"
   }
@@ -35,30 +47,30 @@ resource "aws_security_group" "redis-sg" {
 resource "aws_elasticache_subnet_group" "redis-subnet-group" {
   count      = length(var.redis_nodes_list)
   name       = "${var.redis_nodes_list[count.index]}-${local.cluster_name_suffix}-subnetgroup"
-  subnet_ids = (var.vpc-db-private-subnets != [] ? var.vpc-db-private-subnets : data.aws_subnet_ids.stp-vpc-db-private.ids)
+  subnet_ids = length(var.vpc-db-private-subnets) != 0 ? var.vpc-db-private-subnets : data.aws_subnet_ids.stp-vpc-db-private.ids
 }
 
 resource "aws_elasticache_replication_group" "redis-replica-group" {
-  count = length(var.redis_nodes_list)
-  #checkov:skip=CKV_AWS_191:no not use kms key
-  replication_group_id          = "${var.redis_nodes_list[count.index]}-${local.cluster_name_suffix}"
-  replication_group_description = "Splashtop - Redis Replication Group"
-  engine                        = "redis"
-  engine_version                = var.redis_engine_ver
-  node_type                     = var.redis_node_type
-  automatic_failover_enabled    = var.redis_failover
-  multi_az_enabled              = var.redis_multiaz
-  number_cache_clusters         = var.redis_num_cache_nodes
-  parameter_group_name          = aws_elasticache_parameter_group.redis-para-group[count.index].name
-  port                          = 6379
-  subnet_group_name             = aws_elasticache_subnet_group.redis-subnet-group[count.index].name
-  security_group_ids            = ["${aws_security_group.redis-sg[count.index].id}"]
-  transit_encryption_enabled    = true
-  at_rest_encryption_enabled    = true
-  auth_token                    = random_password.redis_auth_token[count.index].result
-  maintenance_window            = var.redis_maintenance_window[count.index]
-  snapshot_window               = var.redis_snapshot_window[count.index]
-  snapshot_retention_limit      = var.redis_snapshot_retention_limit
+  #checkov:skip=CKV_AWS_191:not use CMK
+  count                      = length(var.redis_nodes_list)
+  replication_group_id       = "${var.redis_nodes_list[count.index]}-${local.cluster_name_suffix}"
+  description                = "Splashtop - Redis Replication Group"
+  engine                     = "redis"
+  engine_version             = var.redis_engine_ver
+  node_type                  = var.redis_node_type
+  automatic_failover_enabled = var.redis_failover
+  multi_az_enabled           = var.redis_multiaz
+  num_cache_clusters         = var.redis_num_cache_nodes
+  parameter_group_name       = aws_elasticache_parameter_group.redis-para-group[count.index].name
+  port                       = 6379
+  subnet_group_name          = aws_elasticache_subnet_group.redis-subnet-group[count.index].name
+  security_group_ids         = ["${aws_security_group.redis-sg[count.index].id}"]
+  transit_encryption_enabled = true
+  at_rest_encryption_enabled = true
+  auth_token                 = random_password.redis_auth_token[count.index].result
+  maintenance_window         = var.redis_maintenance_window[count.index]
+  snapshot_window            = var.redis_snapshot_window[count.index]
+  snapshot_retention_limit   = var.redis_snapshot_retention_limit
 
   auto_minor_version_upgrade = true
   apply_immediately          = true
