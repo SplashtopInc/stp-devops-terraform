281b1347e5fe7d932f349c6172b02bc0174fb9ca
jeff51419 <jeff51419@gmail.com>
2022-09-13 11:57:25 +0800

    add s3 module

---
+++ terraform/module/s3_bucket_for_logs/main.tf
@@ -0,0 +1,27 @@
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.4.0"
+
+  bucket = var.bucket
+  acl    = "log-delivery-write"
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  attach_elb_log_delivery_policy = true # Required for ALB logs
+  attach_lb_log_delivery_policy  = true # Required for ALB/NLB logs
+  # attach_deny_insecure_transport_policy = true # Controls if S3 bucket should have deny non-SSL transport policy attached
+  # attach_require_latest_tls_policy      = true # Controls if S3 bucket should require the latest version of TLS
+
+
+  # S3 bucket-level Public Access Block configuration
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    status     = true
+    mfa_delete = false
+  }
+}
