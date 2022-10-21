1ee17c531a5acea3b7779c0356696e8890ee5ad7
jeff51419 <jeff51419@gmail.com>
2022-10-21 15:28:23 +0800

    add provider and version in module

---
+++ terraform/module/elasticache-redis/provider.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = var.region
+
+  # Only these AWS Account IDs may be operated on by this template
+  assume_role {
+    role_arn = var.assume_role
+  }
+}
+provider "random" {
+}
