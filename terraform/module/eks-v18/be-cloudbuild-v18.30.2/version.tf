0706169829b1bb430c8445a3f4e29526644d1cf8
jeff51419 <jeff51419@gmail.com>
2022-11-22 14:53:01 +0800

    rollback the version file

--- terraform/module/eks-v18/be-cloudbuild-v18.30.2/version.tf
+++ terraform/module/eks-v18/be-cloudbuild-v18.30.2/version.tf
@@ -1,10 +1,18 @@
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 0.13.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.30.0"
+      version = ">= 3.72"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
     }
     random = {
       source  = "hashicorp/random"
