2269832850293bc5175fadfab955ae3596559ce1
jeff51419 <jeff51419@gmail.com>
2023-02-03 11:05:27 +0800

    fix aws lb policy

--- terraform/module/eks-v19/be-app-v19.5.1/iam-eks.tf
+++ terraform/module/eks-v19/be-app-v19.5.1/iam-eks.tf
@@ -536,27 +536,36 @@ data "aws_iam_policy_document" "k8s_aws_lb_controller_trust" {
 
 ## Create an IAM role with policy
 
-resource "aws_iam_role" "k8s_aws_lb_controller_trust_role" {
-  count = var.create ? 1 : 0
-  name  = "${local.name}-k8s_aws_lb_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = "1"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
 
-  inline_policy {
-    name   = "k8s_aws_lb_controller_trust_policy"
-    policy = data.aws_iam_policy_document.k8s_aws_lb_controller_trust.json
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
   }
+}
+
+resource "aws_iam_role" "k8s_aws_lb_controller_trust_role" {
+  count              = var.create ? 1 : 0
+  name               = "${local.name}-k8s_aws_lb_role"
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+
+  # depends_on = [module.eks]
+}
+
+resource "aws_iam_policy" "k8s_aws_lb_controller_trust_policy" {
+  count  = var.create ? 1 : 0
+  name   = "k8s_aws_lb_controller_trust_policy"
+  policy = data.aws_iam_policy_document.k8s_aws_lb_controller_trust.json
+
+  depends_on = [module.eks]
+}
+
+resource "aws_iam_role_policy_attachment" "k8s_aws_lb_controller" {
+  role       = aws_iam_role.k8s_aws_lb_controller_trust_role[0].name
+  policy_arn = aws_iam_policy.k8s_aws_lb_controller_trust_policy[0].arn
 
   depends_on = [module.eks]
 }
