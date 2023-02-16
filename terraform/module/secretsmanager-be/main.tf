6233449fdf717c4e9434dd224e5934e709055df5
jackal.kao <jackal.kao@splashtop.com>
2023-02-16 10:36:02 +0800

    modify secret manager status

--- terraform/module/secretsmanager-be/main.tf
+++ terraform/module/secretsmanager-be/main.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret_version" "this" {
-  count         = data.aws_secretsmanager_secret.name.id ? 1 : 0
+  count         = data.aws_secretsmanager_secret.name.id != "" ? 1 : 0
   secret_id     = data.aws_secretsmanager_secret.name.id
   secret_string = jsonencode(local.secretsmanager_json)
 }
