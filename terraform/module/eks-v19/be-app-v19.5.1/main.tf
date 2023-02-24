4ceb4a870b1c43b79b560b6fa87d124e52991641
jackal.kao <jackal.kao@splashtop.com>
2023-02-24 17:31:31 +0800

    for test

--- terraform/module/eks-v19/be-app-v19.5.1/main.tf
+++ terraform/module/eks-v19/be-app-v19.5.1/main.tf
@@ -13,7 +13,7 @@ provider "kubernetes" {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.name]
+    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this[0].name]
   }
 }
 
