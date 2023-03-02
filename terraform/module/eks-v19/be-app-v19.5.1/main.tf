f0735064a37bd81cf30e7f7498789de27269fedc
jackal.kao <jackal.kao@splashtop.com>
2023-03-02 17:48:43 +0800

    modify for tets

--- terraform/module/eks-v19/be-app-v19.5.1/main.tf
+++ terraform/module/eks-v19/be-app-v19.5.1/main.tf
@@ -447,12 +447,12 @@ module "eks" {
 ################################################################################
 
 data "aws_eks_cluster_auth" "this" {
-  count = var.create ? 1 : 0
+#  count = var.create ? 1 : 0
   name  = module.eks.cluster_name
 }
 
 data "aws_eks_cluster" "this" {
-  count = var.create ? 1 : 0
+#  count = var.create ? 1 : 0
   name  = module.eks.cluster_name
 }
 
@@ -478,7 +478,7 @@ locals {
     users = [{
       name = "terraform"
       user = {
-        token = data.aws_eks_cluster_auth.this[0].token
+        token = data.aws_eks_cluster_auth.this.token
       }
     }]
   }) : "{}"
@@ -486,7 +486,7 @@ locals {
   template_vars = var.create ? {
     cluster_name     = module.eks.cluster_name
     cluster_endpoint = module.eks.cluster_endpoint
-    cluster_ca       = data.aws_eks_cluster.this[0].certificate_authority[0].data
+    cluster_ca       = data.aws_eks_cluster.this.certificate_authority.data
     cluster_profile  = var.profile
     cluster_region   = var.region
   } : {}
