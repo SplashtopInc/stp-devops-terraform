ff07f660fd38e9c2781477af5e01b61d7d882c6b
jackal.kao <jackal.kao@splashtop.com>
2023-03-06 12:41:41 +0800

    test

--- terraform/module/eks-v19/be-app-v19.5.1/main.tf
+++ terraform/module/eks-v19/be-app-v19.5.1/main.tf
@@ -448,12 +448,14 @@ module "eks" {
 
 data "aws_eks_cluster_auth" "this" {
   #count = var.create ? 1 : 0
-  name  = module.eks.cluster_name
+  #name  = module.eks.cluster_name
+  name = var.old_cluster_name == null ? module.eks.cluster_name : var.old_cluster_name
 }
 
 data "aws_eks_cluster" "this" {
   #count = var.create ? 1 : 0
-  name  = module.eks.cluster_name
+  #name  = module.eks.cluster_name
+  name = var.old_cluster_name == null ? module.eks.cluster_name : var.old_cluster_name
 }
 
 locals {
